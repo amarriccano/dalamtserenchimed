bea12381aea0036d945c04c591b58b8edd1f10a7
Amar Batsaikhan <test@Amars-MacBook-Pro.local>
2026-03-08 04:53:01 +0800

    mailer

--- src/core/utils/mailer.ts
+++ src/core/utils/mailer.ts
@@ -1,46 +1,51 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
 })
 
+transporter.verify((error) => {
+  if (error) {
+    console.error('Mailer config error:', error)
+  } else {
+    console.log('Mailer ready')
+  }
+})
+
 export async function sendOrderNotification(order: any) {
-    const itemsList = order.items
-        //TODO .map((i: any) => `${i.title} x${i.quantity} — ${i.price.toLocaleString()} ₮`)
-        .map((i: any) => `${i.title} x${i.quantity}`)
-        .join('\n')
-    console.log('Sending mail to:', process.env.MAIL_USER)
-    console.log('Using pass:', process.env.MAIL_PASS ? 'loaded' : 'MISSING')
-    //TODO assign order Id
-    transporter.verify((error, success) => {
-    if (error) {
-        console.error('Mailer config error:', error)
-    } else {
-        console.log('Mailer ready')
-    }
+  const itemsList = order.items
+    .map((i: any) => `${i.title} x${i.quantity}`)
+    //TODO add items price
+    .join('\n')
+
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to: 'amarbatsaikhan100@gmail.com',
+      subject: `New Order — ${order.customerName}`,
+      //TODO assign orderId
+      text: `
+        New order received!
+
+        Customer:  ${order.customerName}
+        Phone:     ${order.phone}
+        Address:   ${order.address}
+        ${order.notes ? `Notes: ${order.notes}` : ''}
+
+        Items:
+        ${itemsList}
+
+        Total: ${order.total.toLocaleString()} ₮
+      `.trim(),
     })
-    await transporter.sendMail({
-        from: process.env.MAIL_USER,
-        to: "amarbatsaikhan100@gmail.com", // TODO change to variable changeable on admin
-        subject: `New Order — ${order.customerName}`,
-        text: `
-            New order received!
-
-            Order #:   '1234'
-            Customer:  ${order.customerName}
-            Phone:     ${order.phone}
-            Address:   ${order.address}
-            ${order.notes ? `Notes:     ${order.notes}` : ''}
-
-            Items:
-            ${itemsList}
-
-            Total: ${order.total.toLocaleString()} ₮
-                `.trim(),
-    })
-
+    console.log('Mail sent:', info.messageId)
+  } catch (err) {
+    console.error('sendMail error:', err)
+  }
 }
