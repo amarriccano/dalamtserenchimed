9c40f4b525b14db2f92e48adc28b8938af137889
Amar Batsaikhan <test@Amars-MacBook-Pro.local>
2026-03-08 02:22:16 +0800

    order model & create order

--- src/apps/customer/index.ts
+++ src/apps/customer/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import bookRoutes from './routes/book.routes.js';
+import orderRoutes from './routes/order.routes.js'
 
 const customerRouter = Router();
 
-// Mount individual model routes
 customerRouter.use('/books', bookRoutes);
+customerRouter.use('/orders', orderRoutes);
 
 export default customerRouter;
